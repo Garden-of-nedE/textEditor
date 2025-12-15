7d12370e17a82bd022f7fb652d99d59ebe9e2cba
Garden-of-nedE <@Garden-of-nedE>
2025-12-15 21:18:17 +1100

    File editing now operational

--- editor.c
+++ editor.c
@@ -232,6 +232,24 @@ E.row[at].rsize = 0;
    E.numrows++;
 }
 
+void editorRowInsertChar(erow *row, int at, int c) {
+    if (at < 0 || at > row->size) at = row->size;
+    row->chars = realloc(row->chars, row->size + 2);
+    memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+    row->size++;
+    row->chars[at] = c;
+    editorUpdateRow(row);
+}
+
+/*** EDITOR OPERATIONS ***/
+void editorInsertChar(int c) {
+    if (E.cy == E.numrows) {
+        editorAppendRow("", 0);
+    }
+    editorRowInsertChar(&E.row[E.cy], E.cx, c);
+    E.cx++;
+}
+
 /*** FILE I/O ***/
 void editorOpen(char *filename) {
     free(E.filename);
@@ -474,6 +492,10 @@ void editorProcessKeypress() {
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
+            break;
+        
+        default:
+            editorInsertChar(c);
             break;
     }
 }
