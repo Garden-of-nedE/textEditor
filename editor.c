30028ea51dd5018126121f320ac3a861590218ab
Garden-of-nedE <@Garden-of-nedE>
2025-12-15 23:33:09 +1100

    Incremental search implemented
    User is now able to navigate search results using the arrow keys

--- editor.c
+++ editor.c
@@ -65,7 +65,7 @@ struct editorConfig E;
 /*** PROTOTYPES ***/
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** TERMINAL ***/
 void die(const char *s) {
@@ -382,7 +382,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
         if (E.filename == NULL) {
             editorSetStatusMessage("Cancelled Save");
             return;
@@ -410,22 +410,59 @@ void editorSave() {
 }
 
 /*** SEARCH ***/
-void editorSearch() {
-    char *query = editorPrompt("Search: %s (ESC to cancel)");
-    if (query == NULL) return;
+void editorFindCallback(char *query, int key) {
+    static int last_match = -1;
+    static int direction = 1;
 
+    if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
+        return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
+    }
+
+    if (last_match == - 1) direction = 1;
+    int current = last_match;
     int i;
     for (i = 0; i < E.numrows; i++) {
-        erow *row = &E.row[i];
+        current += direction;
+        if (current == -1) current = E.numrows - 1;
+        else if (current == E.numrows) current = 0;
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
         }
     }
-    free(query);
+}
+
+void editorSearch() {
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_coloff = E.coloff;
+    int saved_rowoff = E.rowoff;
+
+
+    char *query = editorPrompt("Search: %s  (Use ESC | Arrows | Enter)", editorFindCallback);
+    if (query) {
+        free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.coloff = saved_coloff;
+        E.rowoff = saved_rowoff;
+    }
 }
 
 /*** APPEND BUFFER ***/
@@ -571,7 +608,7 @@ void editorSetStatusMessage(const char *fmt, ...) {
 }
 
 /*** INPUT ***/
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
     size_t buffsize = 128;
     char *buff = malloc(buffsize);
 
@@ -587,11 +624,13 @@ char *editorPrompt(char *prompt) {
             if (bufflen != 0) buff[--bufflen] = '\0';
         } else if (c == '\x1b') {
             editorSetStatusMessage("");
+            if (callback) callback(buff, c);
             free(buff);
             return NULL;
         } else if (c == '\r') {
             if (bufflen != 0) {
                 editorSetStatusMessage("");
+                if (callback) callback(buff, c);
                 return buff;
             }
         } else if (!iscntrl(c) && c < 128) {
@@ -602,6 +641,8 @@ char *editorPrompt(char *prompt) {
             buff[bufflen++] = c;
             buff[bufflen] = '\0';
         }
+
+        if (callback) callback(buff, c);
     }
 }
 
